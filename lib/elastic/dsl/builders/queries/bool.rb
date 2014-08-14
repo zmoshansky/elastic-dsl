2274bb11108a351cf6c0662cf4526a6256f7d195
zmoshansky <zmoshansky@gmail.com>
2014-08-14 11:56:17 -0700

    Cleaned up log around add_bool_condition, remove options from blank as it would always pass

--- lib/elastic/dsl/builders/queries/bool.rb
+++ lib/elastic/dsl/builders/queries/bool.rb
@@ -27,10 +27,14 @@ module Elastic
 
             # Searches within root_node for a hash keyed by :bool to add or merge conditions with
             def add_bool_condition(type_key, conditions, options)
-              return self if blank?(conditions, options)
+              return self if blank?(conditions)
               root_node = options[:root]
 
-              unless options[:append]
+              if options[:append]
+                # Append a new node
+                raise Elastic::DSL::Errors::InvalidArgument unless root_node.is_a?(Array)
+                root_node << {bool: {type_key => [conditions]}}
+              else
                 # Find or create bool_node
                 begin
                   bool_node = find_node([:bool], root_node)
@@ -40,10 +44,6 @@ module Elastic
                   root_node.is_a?(Array) ? root_node << {bool: bool_node} : root_node[:bool] = bool_node
                 end
                 conditions.is_a?(Array) ? bool_node[type_key] += conditions : bool_node[type_key] << conditions
-              else
-                # Append a new node
-                raise Elastic::DSL::Errors::InvalidArgument unless root_node.is_a?(Array)
-                root_node << {bool: {type_key => [conditions]}}
               end
             end
 
